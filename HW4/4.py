e1794c5b51e367853c9e08eb3d1754b8ac03e899
Yair <y4000r@gmail.com>
2017-06-24 11:31:54 +0300

    A more organized code, that more or less works

--- HW4/4.py
+++ HW4/4.py
@@ -14,6 +14,7 @@ matplotlib.use('Qt4Agg')
 #matplotlib.use('Agg') #in order not to display the plot
 import matplotlib.pyplot as plt
 import os
+import math
 
 
 env_d = 'LunarLander-v2'
@@ -23,31 +24,29 @@ HIDDEN_NEURONS_NO = 15
 OUTPUT_SIZE = 4 #for lunar it should be size 4, for cartPole 2
 LAYERS_NO = 3
 VAR_NO = LAYERS_NO*2
-LEARNING_RATE = 1e-2
-TOTAL_EPISODES = 5000
+LEARNING_RATE = 1e-4
+TOTAL_EPISODES = 100
 PERIOD = 10
 PLOT_PERIOD = 100
 SAVE_PERIOD = 100
-DO_NORMALIZE = False    #TODO: change the name...
-WEIGHTS_FILE = './lunar_weights.pkl'
-LOAD = False
+DO_NORMALIZE = False
+LOAD = True
+SAVE = False
 
 ENVIRONMENT = env_d
+#WEIGHTS_FILE = './'+ENVIRONMENT+'_weights.pkl'
+WEIGHTS_FILE = 'ws.p'
 env = gym.make(ENVIRONMENT)
 env.reset()
 
 
-
-#states - a 4XT matrix, holds all the states from up to the T step
-       #notice it can be a matrix or a vector
-#y = agent(observations)     #TODO: right now y = 0 becaue that's what the function returns... do i really want to do it in a function? i can tale the rest of the code after the y out.
-
+#define different initialize functions for variables
 def InitializeVarXavier(var_name,var_shape):
-    return tf.get_variable(name=var_name, shape=var_shape,
+    return tf.get_variable(name=var_name, shape=var_shape, dtype= tf.float32,
                     initializer=tf.contrib.layers.xavier_initializer())
 
 def InitializeVarRandomNormal(var_name,var_shape):
-    return tf.Variable(tf.random_normal(var_shape))
+    return tf.Variable(tf.random_normal(var_shape), dtype=tf.float32)
 
 #TODO: delete this copy shit later
 
@@ -62,6 +61,7 @@ def bias_variable(name, shape):
 
 '''
 
+#TODO: in case I use the same initializer, delete one
 #here we choose how to initialize
 initializeW = InitializeVarXavier
 initializeb = InitializeVarXavier
@@ -83,91 +83,109 @@ b3 = initializeb("b3",[OUTPUT_SIZE])
 h1 = tf.nn.tanh(tf.matmul(observations, W1) + b1)
 if (LAYERS_NO == 3):
     h2 = tf.nn.tanh(tf.matmul(h1, W2) + b2)
-    y = tf.nn.softmax(tf.matmul(h2, W3) + b3)  # TODO: there is some problem with the softmax
+    y = tf.nn.softmax(tf.matmul(h2, W3) + b3)
 else:
-    y = tf.nn.softmax(tf.matmul(h1, W3) + b3)  # TODO: there is some problem with the softmax
+    y = tf.nn.softmax(tf.matmul(h1, W3) + b3)
 
 tvars = tf.trainable_variables()
-
 # rewards sums from k=t to T:
 rewards_arr = tf.placeholder(tf.float32, [1,None])
 # actions - a mask matrix which filters ys result accordint to the actions that were chosen. see boolean mask here: https://www.tensorflow.org/versions/r0.11/api_docs/python/array_ops/slicing_and_joining#boolean_mask
 actions_mask = tf.placeholder(tf.bool, [None, OUTPUT_SIZE])
-filtered_actions = tf.boolean_mask(y, actions_mask)  # should return a T size vector with correct (chosen) action values
+# should return a T size vector with correct (chosen) action values
+filtered_actions = tf.boolean_mask(y, actions_mask)
 pi = tf.log(filtered_actions)
-#grad_step = tf.divide(tf.reduce_sum(tf.multiply(pi,rewards_arr)),tf.to_float(tf.size(pi)))         #TODO: devide by T
-grad_step = tf.reduce_sum(tf.multiply(pi,rewards_arr))      #TODO: don't devide by T
-Gradients = tf.gradients(-grad_step,tvars)
+#devide by T
+#loss = tf.divide(tf.reduce_sum(tf.multiply(pi,rewards_arr)),tf.to_float(tf.size(pi)))
+#don't devide by T
+loss = tf.reduce_sum(tf.multiply(pi,rewards_arr))
+Gradients = tf.gradients(-loss,tvars)
 
-Gradients_holder = [tf.placeholder(tf.float32) for i in range(VAR_NO) ]
+Gradients_holder = [tf.placeholder(tf.float32) for i in range(VAR_NO)]
 # then train the network - for each of the parameters do the GD as described in the HW.
 train_step = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(zip(Gradients_holder,tvars))
-#train_step = tf.train.AdamOptimizer(1e-2).minimize(grad_step)
 
 #TODO: answer the following questions:
 '''
-new:
-6) maybe i need to zero somethings like he does every once in a while?
-
 for tommorow:
  - take care of the crush where sum(pi)>1 (which crushes the multinomial function) by normalize it
- - don't forget we got more time
 
 things i tried:
  - change the way i initialize
-
-things i didn't try:
- - reduce mean
- - change the way i normalize
- - change the reduce-prize function
- - change the way i process the rewards
-
 things to check tommorow:
  - why Aran entered minus sum to the gradient function?
  - this code that solves the problem: https://gym.openai.com/evaluations/eval_FbKq5MxAS9GlvB7W6ioJkg
  - other solutions: https://gym.openai.com/envs/LunarLander-v2
 '''
 
+#TODO: what's left?
+'''
+1) finish clean code
+2) check for a better initializer
+3) look up softmax parameters
+4) check on nova
+5) submit the HM
+'''
 
-#we assume here we get the array in the right order
+
+#we assume here we get the array in the right order, so each sum is indeed being multiply with the right factor
 def decrese_rewards(rewards):
     gama = 0.99
     dec_arr = np.array([gama**(len(rewards)-t) for t in range(len(rewards))])
-    res = np.multiply(rewards,dec_arr);
+    res = np.multiply(rewards,dec_arr)
     return res
 
+#returns objects to hold the gradients of each trainable variable
 def get_empty_grads_sums():
     grads_sums = tf.trainable_variables()
     for i, val in enumerate(grads_sums):
         grads_sums[i] = 0
     return grads_sums
 
+def simple_normalize_vector(v):
+    res = np.divide(v,np.sum(v))
+    return res
+
+def pick_random_action_manually(actions):
+    r = 1
+    while(r==1):
+        r = np.random.uniform()
+    m = np.max(actions)
+    l = len(actions)
+    if( r < m):
+        res = [ 0 if actions[i] < m else 1 for i in range(l)]
+    else:
+        index = math.floor(r*l)
+        res = [0 if i != index else 1 for i in range(l)]
+    return res
+
+
 init = tf.global_variables_initializer()
 def main(argv):
     rewards, states, actions_booleans = [], [], []
-    episode_number,total_reward,running_reward,reward_for_plot,save_reward = 0,0,0,0,None
+    episode_number,period_reward,running_reward,reward_for_plot,save_reward = 0,0,0,0,None
     steps = 0
-    # TODO: for debug
-    '''
-    action_sum = np.array([[0.0,0.0]])
-    action_probs_arr = []
-    '''
+    manual_prob_use = 0             #TODO: for debug
     #for plotting:
     rewards_per_episode = [0 for i in range(TOTAL_EPISODES//PLOT_PERIOD)]
 
+
     with tf.Session() as sess:
-        saver = tf.train.Saver()
+        sess.run(init)
+        #saver = tf.train.Saver()
+
         #check if file is not empty
         if(os.path.isfile(WEIGHTS_FILE) and LOAD):
-            #f = open(WEIGHTS_FILE,'rb')
-            #r1 = sess.run(tvars)
+            '''
+            #Load with Tensorflow saver
             saver.restore(sess,WEIGHTS_FILE)
-            #zip(sess.run(tvars),pkl.load(f))
-            r2 = sess.run(tvars)
-            #f.close()
+            '''
+            #Load with shmickle
+            f = open(WEIGHTS_FILE,'rb')
+            for var, val in zip(tvars,pkl.load(f)):
+                sess.run(tf.assign(var,val))
+            f.close()
             print("loaded weights successfully!")
-        else:
-            sess.run(init)
 
         #creates file if it doesn't exisits:
         if(not os.path.isfile(WEIGHTS_FILE)):
@@ -175,100 +193,91 @@ def main(argv):
             print("created file sucessfully!")
 
         obsrv = env.reset() # Obtain an initial observation of the environment
-        grads_sums = get_empty_grads_sums()
+        grads_sums = get_empty_grads_sums()     #initialize the gradients holder for the trainable variables
 
-
-        while episode_number <= TOTAL_EPISODES:
-            #append relevant observation to action to states
+        while (episode_number <= TOTAL_EPISODES) or (reward_for_plot/PLOT_PERIOD > 200):
+            #append the relevant observation to folloeing action, to states
             states.append(obsrv)
+            #modify the observation to the model
             modified_obsrv = np.reshape(obsrv, [1, INPUT_SIZE])
-            '''
-            temp = sess.run(tvars)      #TODO: for debug
-            th1 = sess.run(h1,feed_dict={observations: modified_obsrv})
-            th2 = sess.run(h2,feed_dict={observations: modified_obsrv})
-            th3 = sess.run(y,feed_dict={observations: modified_obsrv})
-
-            '''
-
             # Run the policy network and get a distribution over actions
             action_probs = sess.run(y,feed_dict={observations: modified_obsrv})
-            actions_booleans.append(np.random.multinomial(1, action_probs[0]))
-
-            '''
-            if(ENVIRONMENT == cart_pole_env):
-                action = 0 if action_probs[[0]] < 0.5 else 1
-            else:
-            '''
+            # np.random.multinomial cause problems
+            try:
+                m_actions = np.random.multinomial(1, action_probs[0])
+            except:
+                '''
+                print("you are a worthless piece of shit: "+str(simple_normalize_vector(action_probs[0])))
+                print("that's it's sum: "+str(sum(simple_normalize_vector(action_probs[0]))))
+                raise BaseException("bye-bye fuckers!")
+                '''
+                m_actions = pick_random_action_manually(action_probs[0])
+                manual_prob_use += 1
+            # Saves the selected action for a later use
+            actions_booleans.append(m_actions)
+            #index of the selected action
             action = np.argmax(actions_booleans[-1])
             # step the environment and get new measurements
             obsrv, reward, done, info = env.step(action)
-            #add reward to rewards and obsrv to states
+            #add reward to rewards for a later use in the training step
             rewards.append(reward)
-            #action_sum += np.array(action_probs)
             steps += 1
             if done:
-                #create the rewards sums array and reverse again
+                #create the rewards sums of the reversed rewards array
                 rewards_sums = np.cumsum(rewards[::-1]);
                 #normalize prizes and reverse
                 rewards_sums = decrese_rewards(rewards_sums[::-1])
                 rewards_sums -= np.mean(rewards_sums)
-                #rewards_sums = np.divide(rewards_sums, np.sum(rewards_sums))
-                #rewards_sums = decrese_rewards(np.divide(rewards_sums[::-1],np.sum(rewards_sums)))
                 rewards_sums = np.divide(rewards_sums, np.std(rewards_sums))
                 modified_rewards_sums = np.reshape(rewards_sums, [1, len(rewards_sums)])
                 #modify actions_booleans to be an array of booleans
-                #debug_ab = actions_booleans
                 actions_booleans = np.array(actions_booleans)
                 actions_booleans = actions_booleans == 1
-                #gradients for check.
-                # TODO: the unused are for debug
-                '''
-                th3 = sess.run(y, feed_dict={observations: states})
-                fa = sess.run(filtered_actions, feed_dict={observations: states,actions_mask:actions_booleans,rewards_arr: modified_rewards_sums })
-                pi_debug = sess.run(pi, feed_dict={observations: states,actions_mask:actions_booleans,rewards_arr: modified_rewards_sums })
-                loss_debug = sess.run(grad_step, feed_dict={observations: states,actions_mask:actions_booleans,rewards_arr: modified_rewards_sums })
-                th3 = sess.run(y, feed_dict={observations: states})
-                filt_act = sess.run(filtered_actions, feed_dict={observations: states,actions_mask:actions_booleans,rewards_arr: modified_rewards_sums })
-                action_probs_arr = np.array(action_probs_arr)
-                is_equal = np.add(th3, -action_probs_arr)
-                s_is_equal = sum(sum(is_equal))
-
-                '''
-
+                #gradients for current episode
                 grads = sess.run(Gradients, feed_dict={observations: states,actions_mask:actions_booleans,rewards_arr: modified_rewards_sums })
                 grads_sums += np.array(grads)
-                total_reward += sum(rewards)
+
+                #reward counters for printing and plotting.
+                period_reward += sum(rewards)
                 reward_for_plot += sum(rewards)
-                running_reward = total_reward if running_reward ==0 else running_reward * 0.99 + total_reward * 0.01
-
-                if(episode_number%SAVE_PERIOD==0):
-                    if(save_reward is None or save_reward<(reward_for_plot/ SAVE_PERIOD)):
-                        save_reward = (reward_for_plot/ SAVE_PERIOD)
-                        saver.save(sess,WEIGHTS_FILE)
-                        #save with shmickle
-                        #f = open(WEIGHTS_FILE,'wb')
-                        #pkl.dump(sess.run(tvars),f)
-                        #f.close()
-                        print('saved file successfully!')
-
-                if(episode_number%PERIOD==0):
-                    # take the train step
-                    sess.run(train_step, feed_dict={Gradients_holder[i]: grads_sums[i] for i in range(VAR_NO)})
-                    print ('Episode No. %f., Steps No. %d,   Episodes average reward %f., Total average reward %f.' % (episode_number, steps, total_reward / PERIOD, running_reward / PERIOD))
-                    #print('Average reward for episode %f.  Total average reward %f.' % (total_reward / PERIOD, running_reward / PERIOD))
-                    #print("actions_sum = ", action_sum)
-                    total_reward = 0
-                    grads_sums = get_empty_grads_sums()
-                if(episode_number%PLOT_PERIOD == 0):
-                    rewards_per_episode[episode_number // PLOT_PERIOD-1] = reward_for_plot / PLOT_PERIOD
-                    reward_for_plot = 0
+                #running_reward = period_reward if running_reward ==0 else running_reward * 0.99 + period_reward * 0.01     #TODO: delete
 
                 episode_number += 1
+                #saves the best weights:
+                if(episode_number%PLOT_PERIOD==0):
+                    if((save_reward is None or save_reward<(reward_for_plot/ PLOT_PERIOD)) and SAVE):
+                        save_reward = (reward_for_plot/ PLOT_PERIOD)
+                        '''
+                        #save with Tensorflow saver:
+                        #saver.save(sess,WEIGHTS_FILE)
+                        '''
+                        #save with shmickle
+                        f = open(WEIGHTS_FILE,'wb')
+                        pkl.dump(sess.run(tvars),f)
+                        f.close()
+                        print('Saved file successfully!')
+                        print('Current best result for %d episodes: %f.' %(PLOT_PERIOD,reward_for_plot/ PLOT_PERIOD))
+
+                #Do the training step
+                if(episode_number%PERIOD==0):
+                    running_reward += period_reward / PERIOD
+                    # take the train step
+                    sess.run(train_step, feed_dict={Gradients_holder[i]: grads_sums[i] for i in range(VAR_NO)})
+                    print ('Episode No. %d, Steps No. %d,   Episodes average reward %f., Total average reward %f.' % (episode_number, steps, period_reward / PERIOD, running_reward / (episode_number//PERIOD)))
+                    print('Amount of manually drawing this period: %d' %(manual_prob_use))
+                    period_reward = 0
+                    manual_prob_use = 0
+                    grads_sums = get_empty_grads_sums()
+
+                #add reward to plot array
+                if(episode_number%PLOT_PERIOD == 0):
+                    rewards_per_episode[(episode_number // PLOT_PERIOD)-1] = reward_for_plot / PLOT_PERIOD
+                    reward_for_plot = 0
+
+                #nullify relevant vars and updates episode number.
                 rewards, states, actions_booleans = [], [], []
-                # TODO: for debug
-                #action_probs_arr = []
-                obsrv = env.reset()
                 steps = 0
+                obsrv = env.reset()
 
     IS_NORMALIZED = ""
     if(DO_NORMALIZE):
@@ -277,11 +286,7 @@ def main(argv):
     plt.title(ENVIRONMENT+" Rewards Average per "+str(PLOT_PERIOD)+" Episodes for "+IS_NORMALIZED+" NN with "+str(LAYERS_NO)+" layers")
     plt.savefig("..\\..\\graphs\\rewards_for_"+ENVIRONMENT+"_period_"+str(PLOT_PERIOD)+"_layers_"+str(LAYERS_NO)+"_"+IS_NORMALIZED+"_episodes_"+str(TOTAL_EPISODES)+".png")
 
+    print("Finised running after %d episoded! best average reward for %d episodes is %f."% (episode_number,PLOT_PERIOD,save_reward))
 
 if __name__ == '__main__':
     tf.app.run()
-    # save with pickle
-    #TODO: doesn't work...
-    #f = open("tvars.pkl", "wb")
-    #pickle.dump(tvars, f)
-    #f.close()
